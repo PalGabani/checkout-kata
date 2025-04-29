9c462f0aa15a2152d1e7dc4a9f1df013b2b660a4
PalGabani <palgabani03@gmail.com>
2025-04-29 09:50:04 +0530

    final commit

--- cart/checkout.py
+++ cart/checkout.py
@@ -7,14 +7,21 @@ class Checkout:
         'C': 20,
         'D': 15
     }
-
     DISCOUNTS = {
-        'A': {'qty': 3, 'price': 130},
+        'A': ({'qty': 3, 'price': 130},{'qty':5,'price':200}),
         'B': {'qty': 2, 'price': 45}
     }
 
     def __init__(self, items):
         self.items = items.upper() if items else ""
+
+
+    def check_discount_prices(self):
+        for i,j in self.DISCOUNTS:
+            if len(j)>1:
+                return list(j)
+            return j
+        
 
     def total(self):
         item_count = {}
@@ -22,14 +29,33 @@ class Checkout:
             if item in self.PRICES:
                 item_count[item] = item_count.get(item, 0) + 1
 
+
         total_price = 0
         for item, quantity in item_count.items():
+        
+            
             if item in self.DISCOUNTS:
                 offer = self.DISCOUNTS[item]
-                sets = quantity // offer['qty']
-                remainder = quantity % offer['qty']
-                total_price += sets * offer['price'] + remainder * self.PRICES[item]
+
+                for offers in offer:
+                    if offers['qty']==quantity:
+                        sets = quantity // offer['qty']
+                        remainder = quantity % offer['qty']
+                        total_price += sets * offer['price'] + remainder * self.PRICES[item]
+                    else:
+                        while quantity>0:
+                            if offers['qty']>quantity:
+                                break
+                            if offer['qty']!=quantity:
+                                continue
+                            discounted_price=0
+                            sets = quantity // offers['qty']
+                            remainder = quantity % offers['qty']
+                            discounted_price+=sets*
+                            quantity-=1
+
             else:
                 total_price += quantity * self.PRICES[item]
 
-        return total_price
+        return total_price+dis
+
